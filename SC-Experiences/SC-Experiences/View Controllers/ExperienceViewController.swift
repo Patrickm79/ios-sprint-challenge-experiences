2b56df9f007a23684eab7576d47d7a48c6ad1c42
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-06 18:42:19 -0500

    Setting up experience VC + Adding stubs and importing relevant framework

---
+++ SC-Experiences/SC-Experiences/View Controllers/ExperienceViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ExperienceViewController.swift
+//  SC-Experiences
+//
+//  Created by Patrick Millet on 6/6/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+import AVFoundation
+import CoreLocation
+
+class ExperienceViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var posterImageButton: UIButton!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var recordAudioButton: UIButton!
+    @IBOutlet weak var playRecordingButton: UIButton!
+    
+    
+    
+    
+    
+}
